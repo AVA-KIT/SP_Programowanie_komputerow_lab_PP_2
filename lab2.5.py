207fcbf3dd025aa86abf981f3ecfd4c207cab2ac
AVA-KIT <gazda.edu@gmail.com>
2020-01-20 10:46:03 +0100

    Zadanie 5

---
+++ lab2.5.py
@@ -0,0 +1,7 @@
+liczba=float(input("Podaj liczbę: "))  #pobranie liczby od użytkownika
+
+#sprawdzamy czy liczba jest parzysta
+if liczba % 2 == 0:                                
+    print('Liczba ', liczba, ' jest parzysta')
+else:
+    print('Liczba ', liczba, ' jest nieparzysta')
