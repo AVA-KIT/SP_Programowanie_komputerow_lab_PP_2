853da4b07d6787fef5c822c0cbed67f684c15aca
AVA-KIT <gazda.edu@gmail.com>
2020-01-20 10:04:44 +0100

    Zadanie 2

--- lab2.2.py
+++ lab2.2.py
@@ -1,12 +1,12 @@
 chanels = {'Google':1480, 'Email':300, 'Natural Traffic':440, 'TV Spot':700} #obiekt dictionary o nazwie chanels 
 
-print(chanels['Email'])
+print(chanels['Email']) #Wyświetlanie wartość skojarzoną z kluczem "Email"
 
 chanelsUpdate = {'Natural Traffic':520, 'News':600} #nowy słownik chanelsUpdate
 
 chanels.update(chanelsUpdate) #Aktualizacja słownika chanels wartościami z chanelsUpdate
 
-print(chanels.keys())
+print(chanels.keys()) #Wyświetlanie wszystkich kluczy z chanels
 
 del chanels['Email'] #Usunięcie wartości 'Email' ze słownika
 
