ad809d18bbdef1c7f496dcc4e18632cbbc326ec3
AVA-KIT <gazda.edu@gmail.com>
2020-01-21 15:40:49 +0100

    Zadanie 6

---
+++ lab2.6.py
@@ -0,0 +1,20 @@
+a = 0
+
+for i in range(6):
+    print(a, end=" ")
+    a += 1
+
+print()
+
+b = 0
+
+while b <= 5:
+    print(b, end=" ")
+    b += 1
+
+print()
+
+lista = [0, 1, 2, 3, 4, 5]
+
+for i in range(len(lista)):
+    print(i, end=" ")
