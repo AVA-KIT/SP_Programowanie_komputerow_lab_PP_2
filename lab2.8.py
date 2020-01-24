301922cf3ab30b2183791fbacbc17626b2ea705d
AVA-KIT <gazda.edu@gmail.com>
2020-01-24 11:55:29 +0100

    Zadanie 8

---
+++ lab2.8.py
@@ -0,0 +1,13 @@
+lista1 = [1,2,3]
+lista2 = ['a','b','c']
+
+for i in range(3):
+    print(lista1[0], lista2[i])
+
+for i in range(3):
+    print(lista1[1], lista2[i])
+
+for i in range(3):
+    print(lista1[2], lista2[i])
+
+print()
