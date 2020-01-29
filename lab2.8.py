91243eb57b4994bf82dcc372c98b8e460b0d53da
AVA-KIT <gazda.edu@gmail.com>
2020-01-29 10:46:45 +0100

    Zadanie 8

--- lab2.8.py
+++ lab2.8.py
@@ -1,3 +1,25 @@
+#Pierwszy sposób uzyskania ciągu
+lista = ['1 a', '1 b', '1 c','2 a','2 b', '2 c', '3 a', '3 b', '3 c']
+
+for i in lista:
+    print(i)
+
+print()
+
+#Drugi sposób uzyskania ciągu
+lista = ['a', 'b', 'c']
+
+for i in lista:
+    print(1, i)
+for i in lista:
+    print(2, i)
+for i in lista:
+    print(3, i)
+
+
+print()
+
+#Trzeci sposób uzyskania ciągu
 lista1 = [1,2,3]
 lista2 = ['a','b','c']
 
